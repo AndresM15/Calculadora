9fec84612c1f2475c00e0df387a83d843c4e3617
Andres Montoya Ocampo <andres.montoyao@autonoma.edu.co>
2024-09-03 16:33:14 -0500

    Elimine el acumulado

--- src/Main.java
+++ src/Main.java
@@ -3,43 +3,36 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Operaciones operaciones = new Operaciones();
-        Scanner scanner = new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in);
         String opcion = "";
         System.out.println("Bienvenido a la calculadora "+"/n");
-        System.out.println("1: Sumar, 2: Restar, 3: Multiplicar,4: Dividir, 5: Salir, 6:Acumulado");
-        String opcion = scanner.nextLine();
-        double acumulado=0;
+        System.out.println("1: Sumar, 2: Restar, 3: Multiplicar,4: Dividir, 5: Salir");
+        String opcion = entrada.nextLine();
         while (!opcion.equals("5")) {
             if (opcion.equals("1")) {
                 System.out.println("Ingrese el primer numero");
-                float num1 = scanner.nextFloat();
+                float num1 = entrada.nextFloat();
                 System.out.println("Ingrese el segundo numero");
-                float num2 = scanner.nextFloat();
+                float num2 = entrada.nextFloat();
                 System.out.println(operaciones.sumar(num1,num2));
-                acumulado=operaciones.sumar(num1,num2);
             }else if (opcion.equals("2")) {
                 System.out.println("Ingrese el primer numero");
-                float num1 = scanner.nextFloat();
+                float num1 = entrada.nextFloat();
                 System.out.println("Ingrese el segundo numero");
-                float num2 = scanner.nextFloat();
+                float num2 = entrada.nextFloat();
                 System.out.println(operaciones.restar(num1,num2));
-                acumulado=operaciones.restar(num1,num2);
             }else if (opcion.equals("3")) {
                 System.out.println("Ingrese el primer numero");
-                float num1 = scanner.nextFloat();
+                float num1 = entrada.nextFloat();
                 System.out.println("Ingrese el segundo numero");
-                float num2 = scanner.nextFloat();
+                float num2 = entrada.nextFloat();
                 System.out.println(operaciones.multiplicar(num1,num2));
-                acumulado=operaciones.multiplicar(num1,num2);
             }else if (opcion.equals("4")) {
                 System.out.println("Ingrese el primer numero");
-                float num1 = scanner.nextFloat();
+                float num1 = entrada.nextFloat();
                 System.out.println("Ingrese el segundo numero");
-                float num2 = scanner.nextFloat();
+                float num2 = entrada.nextFloat();
                 System.out.println(operaciones.dividir(num1,num2));
-                acumulado=operaciones.dividir(num1,num2);
-            }else if(opcion.equals("5")) {
-                System.out.println(acumulado);
             }
         }
 
